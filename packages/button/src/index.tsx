d959801c6a79897e6c27f315f2893eb98cc75c64
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-03-27 21:29:53 +0530

    feat: add button component with TypeScript configuration and Jest setup

---
+++ packages/button/src/index.tsx
@@ -0,0 +1,11 @@
+export type ButtonProps = {
+    children: React.ReactNode;
+}
+
+const Button = ({children}: ButtonProps) => {
+    return(
+        <button>{children}</button>
+    )
+};
+
+export default Button;
