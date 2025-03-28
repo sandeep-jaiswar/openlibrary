d4eab7f4b9d8b57d1ba03be5706804408ba33469
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-03-28 13:26:35 +0530

    feat: bump jest package to version 0.0.4 and update changelog

--- packages/jest/src/jest.config.ts
+++ packages/jest/src/jest.config.ts
@@ -2,71 +2,18 @@ import type { Config } from "jest";
 
 const config: Config = {
   automock: true,
-
   clearMocks: true,
-
   collectCoverage: true,
-
   coverageDirectory: "coverage",
-
   coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
-
   coverageProvider: "v8",
-
   coverageReporters: ["json", "text", "lcov", "clover"],
-
-  fakeTimers: {
-    enableGlobally: true,
-  },
-
-  moduleDirectories: ["node_modules"],
-
-  moduleFileExtensions: [
-    "js",
-    "mjs",
-    "cjs",
-    "jsx",
-    "ts",
-    "tsx",
-    "json",
-    "node",
-  ],
-
-  notify: true,
-
-  resetMocks: true,
-
-  resetModules: true,
-
-  restoreMocks: true,
-
-  roots: ["<rootDir>"],
-
-  runner: "jest-runner",
-
-  setupFiles: [],
-
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
-
-  slowTestThreshold: 5,
-
   testEnvironment: "jsdom",
-
-  testEnvironmentOptions: {},
-
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
-
-  testPathIgnorePatterns: ["\\\\node_modules\\\\"],
-
-  testRegex: [],
-
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-
-  transformIgnorePatterns: ["\\\\node_modules\\\\", "\\.pnp\\.[^\\\\]+$"],
-
-  watchman: true,
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
 };
 
 export default config;
